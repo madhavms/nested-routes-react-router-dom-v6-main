e25e4afe54e5673048cf0ba280a93a7e3a4d0cb5
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-04-05 14:13:09 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-    publicPath: "/react-routing-app"
+    filename: "bundle.js"
   },
   module: {
     rules: [
